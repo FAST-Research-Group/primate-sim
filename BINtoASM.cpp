d12acc1c423e6369f5c08b15ba5886eed313d90e
MirceaNT <mirceatatulescu@utexas.edu>
2024-04-04 12:54:44 -0500

    Some I-Type and U-Type instructions were added to result vector

--- BINtoASM.cpp
+++ BINtoASM.cpp
@@ -12,6 +12,7 @@ vector<string> hexadecimal;
 vector<string> binary;
 vector<string> assembly;
 
+
 /*
  * This function will put all of the assembly functions in the assembly vector
  *
@@ -47,15 +48,16 @@ void read(const string& filename, vector<string> destination){
  * This function will take in a hex number and convert it into a string of 0's and 1's
  * the input is the hexadecimal vector, and the index of the hexadecimal vector
  * Input: Hexadecimal vector
- * Output: Binary vector
+ * Output: Global variable, binary, that stores all binary numbers with new line characters
  *
  * SHOULD WORK
  * ******** BIG SWITCH STATEMENT*********
  */
-vector<string> Convert_Binary(vector<string> input, vector<string> output){
+void Convert_Binary(vector<string> input){
     int i = 0;
     int j = 0;
     string result;
+    // return this. Should be in binary with new lines for each instruction
 
     while(i < input.size()){
         string hex_num = input.at(i);
@@ -119,13 +121,29 @@ vector<string> Convert_Binary(vector<string> input, vector<string> output){
                     break;
             }
         }
-        output.push_back(result);
+        binary.push_back(result);
         result = '\0';
     }
-
 }
 
 
+/*
+ * This function will convert a binary string into a 2's complement int and back to string
+ * input: binary string
+ * output: 2's complement string
+ */
+string bin_integer_signed(string binary_string){
+    int number = stoi(binary_string,0,2);
+    if(binary_string[0] == '1'){
+        number -= (1<<binary_string.length());
+    }
+    return to_string(number);
+}
+
+string bin_integer_unsigned(const string& binary_string){
+    int number = stoi(binary_string,0,2);
+    return to_string(number);
+}
 
 /*
  * This function takes
@@ -135,15 +153,31 @@ vector<string> Convert_Binary(vector<string> input, vector<string> output){
  * *******NOTE FOR LATER********
  * The assembly vector might not be needed as it can be initialized in the function and returned
  */
-vector<string> Convert_Assembly(vector<string> input, vector<string> output){
+void Convert_Assembly(vector<string> input, vector<string> output){
     //the first type will correspond with the first binary input; needed to simplify my conversion to
     //assembly
     vector<char> type; //R,I,S,B,U,J,C,N ***** C for custom ******* N for New Line **********
-    string opcode;
+    string opcode; //holds the last 7 bits of an instruction
+    string func3; //this will be the funct 3 bits
+    string immediate; //this will hold the immediate bits
+    string rd; // bits 7-11
+    string rs1; // bits 15-19
+    string rs2; // bits 24-20
+
+
+    string result; //this will be the final instruction
     int i = 0;
+    int j;
     while(i < input.size()){
-        const string& current = input.at(i);
-        for(int j = 25; j < 32; j++){
+        result = "\0";
+        func3 = "\0";
+        rd = "\0";
+        rs1 = "\0";
+        opcode = "\0";
+
+        const string& current = input.at(i); //current binary instruction
+
+        for(j = 25; j < 32; j++){
             if(j < current.length()){
                 opcode += current[j];
             }
@@ -152,11 +186,127 @@ vector<string> Convert_Assembly(vector<string> input, vector<string> output){
             }
 
         } //this takes the opcode unless there is a new line
-        if(opcode == "0010011" || opcode == "0000011" || opcode == "0001111" || opcode == "1110011"){
-            type.emplace_back('I');
+
+
+        if(opcode == "0010011" || opcode == "0000011" || opcode == "0001111" || opcode == "1110011"){ //type I instruction (immediate)
+            // these for loops will generate the proper numbers for me to append to the result before storing it in the assembly
+            for(j = 17; j < 20; j ++){
+                func3 += current[j];
+            }
+            for(j = 0; j < 12;j++){
+                immediate += current[j];
+            }
+            for(j=12;j<17;j++){
+                rs1 += current[j];
+            }
+            for(j = 20; j < 25; j++){
+                rd += current[j];
+            }
+
+
+            if(opcode == "0010011"){
+                if(func3 == "000"){
+                    result = "ADDi,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+                }
+                else if(func3 == "010"){
+                    result += "SLTI,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+
+                }
+                else if(func3 == "011"){
+                    result += "SLTIU,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+                }
+                else if(func3 == "111"){
+                    result += "ANDi,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+                }
+                else if(func3 == "110"){
+                    result += "ORi,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+                }
+                else if(func3 == "100"){
+                    result += "XORi,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate));
+                    result += ";";
+                }
+                else if(func3 == "001"){
+                    result += "SLLi,R";
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate.substr(7)));
+                    result += ";";
+                }
+                else{
+                    if(func3[1] == '1'){
+                        result += "SRAi,R";
+                    }
+                    else{
+                        result += "SRLi,R";
+                    }
+                    result.append(bin_integer_unsigned(rd));
+                    result += ",R";
+                    result.append(bin_integer_unsigned(rs1));
+                    result += ",";
+                    result.append(bin_integer_signed(immediate.substr(7)));
+                    result += ";";
+                }
+            }
         }
         else if(opcode == "0110111" || opcode == "0010111"){
-            type.emplace_back('U');
+            for(j = 0; j < 20;j++){
+                immediate += current[j];
+            }
+            for(j = 20;j < 25;j++){
+                rd += current[j];
+            }
+
+            if(opcode == "0110111"){
+                result += "LUI,R";
+                result.append(bin_integer_unsigned(rd));
+                result+= ",";
+                result.append(bin_integer_signed(immediate));
+                result += ";";
+            }
+            else if(opcode == "0010111"){
+                result += "AUIPC,R";
+                result.append(bin_integer_unsigned(rd));
+                result += ",";
+                result.append(bin_integer_signed(immediate));
+                result += ";";
+            }
         }
         else if(opcode == "0110011"){
             type.emplace_back('R');
@@ -176,22 +326,53 @@ vector<string> Convert_Assembly(vector<string> input, vector<string> output){
         else{
             type.emplace_back('N');
         }
+        assembly.push_back(result);
+
     }
 
+
+    for(i = 0; i<type.size();i++){
+        const string& bin = input.at(i);
+        const string& current = input.at(i);
+        for(int l = 25; l < 32; l++){
+            if(l < current.length()){
+                opcode += current[l];
+            }
+            else{
+                break;
+            }
+
+        } //this takes the opcode unless there is a new line
+
+       if(type.at(i) == 'U'){
+            return;
+       }
+       else if (type.at(i) == 'R'){
+            return;
+       }
+       assembly.push_back(result);
+       func3 = "\0";
+    }
 
 }
 
 /*
  * This function will write each assembly instruction in the Assembly.txt file
  * input: vector with assembly instructions
+ * output new file with assembly instructions
  */
-void write(){
-
+void write(const string& input_file){
+    ofstream outputFile(input_file);
+    for(int i = 0; i<assembly.size(); i++){
+        outputFile<<assembly.at(i)<<flush;
+    }
 }
 
 // ADDi,R0,X4,511
 //   0001 1111 1111 00100 000 00000 0010011
 int main() {
+
+    ofstream outputFile("Assembly.txt"); // in order to write to Assembly.txt use outputFile<<"message"<<endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
