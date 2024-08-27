a077e8d3ce1afe8148ec2852cfbd0b58aac71f86
MirceaNT <mirceatatulescu@utexas.edu>
2024-08-27 12:13:33 -0500

    Machine State, Main, and debugged Parsing

---
+++ inc/Register.hpp
@@ -0,0 +1,30 @@
+class Register
+{
+    uint32_t num_elements;
+    uint32_t *raw_ints;
+
+public:
+    Register(int bitsize);
+    Register(const Register &);
+    Register &operator=(const Register &);
+    ~Register();
+
+    Register operator+(const int32_t);
+    Register operator+(const Register &);
+    Register operator-(const int32_t);
+    Register operator-(const Register &);
+    Register operator<<(const int32_t);
+    Register operator<<(const Register &);
+    Register operator^(const int32_t);
+    Register operator^(const Register &);
+    Register operator|(const int32_t);
+    Register operator|(const Register &);
+    Register operator&(const int32_t);
+    Register operator&(const Register &);
+
+    bool operator<(const Register &);
+    bool operator<(const int32_t);
+    bool operator>=(const Register &);
+
+    bool unsigned_lt(const int32_t);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <string>
 #include "Instruction.cpp"
+#include <cstdint>
+#include "MachineState.cpp"
 
 using namespace std;
 
@@ -36,7 +38,9 @@ vector<vector<Instruction>> read(const string &filename)
 
         while (iss >> word)
         {
-            Instruction CurInstruct(stoi(word, 0, 16));
+            uint32_t unum = (uint32_t)(stoul(word, nullptr, 16));
+            int num = static_cast<int>(unum);
+            Instruction CurInstruct(num);
             CurPacket.push_back(CurInstruct);
         }
     }
@@ -72,14 +76,74 @@ void get_data(Instruction dat)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    /*Pseudo code to be implemented as suggested by Kayvan and interpreted by me
+    // error if not enough arguments
+    // error if too many arguments
+
+    // read in the file
+
+    // parse the file
+
+    // create initial machine state (Machine state new class)
+
+    // create Backend instance (vector<FunctionalUnit>)
+
+    // loop program until finished
+    while(!halted) {
+
+
+        // fetch - already done by parsing the file
+
+
+        // decode - part of the execute since instruction will go to functional unit
+
+
+        // execute - function proc(instruction) will be run, method written by someone else, can be modified to run each instruction of a vector or one VLIW
+        !!!!!!! Throw a warning if the destination registers of a VLIW are the same to help debug the compiler LLVM stuff in the future
+        !!!!!!! An error might occur from consts.hpp
+
+        // writeback - TBD who updates the machine state
+    }
+
+    // print final machine state (eventually)
+    */
+
+    if (argc != 2) // error if wrong amount of arguments arguments
     {
         cerr << "Usage: " << argv[0] << " <file_path>" << endl;
         return 1;
     }
 
+    // current bug with stoi that doesn't work with the TCP file
+
     string filePath = argv[1];
-    vector<vector<Instruction>> instructions = read(filePath);
+    vector<vector<Instruction>> instructions = read(filePath); // reading an parsing done here
+
+    MachineState test = MachineState(0); // initial machine state (!!!!!!!!!!!! This will be a bug that needs to be changed)
+
+    // Throw warnings if same destination
+    for (int i = 0; i < instructions.size(); i++)
+    {
+        for (int j = 0; i < instructions.at(i).size() - 1; j++)
+        {
+            for (int k = 1; k < instructions.at(i).size(); k++)
+                if (instructions.at(i).at(j).get_rd() == instructions.at(i).at(k).get_rd())
+                {
+                    cout << j << " and " << k << " have the same destination register in Instruction " << i << endl;
+                }
+        }
+    }
+
+    while (test.running)
+    {
+        for (int i = 0; i < INSTRS_PER_PACKET; i++)
+        {
+            proc_instruction(instructions.at(i));
+        }
+    }
+
+    /*
+    // starting here 1.0, this creates an output file that I needed in order to test that I was parsing the instructions properly
 
     ofstream outfile("text.txt");
     if (!outfile.is_open())
@@ -94,13 +158,16 @@ int main(int argc, char *argv[])
         int j = 0; // Reset j for each new outer loop iteration
         while (j < instructions.at(i).size())
         {
-            outfile << instructions.at(i).at(j).get_instruction() << endl;
+            outfile << instructions.at(i).at(j).get_instruction() << " ";
             j++;
         }
+        outfile << endl;
         i++;
     }
 
     outfile.close();
+    // ending here 1.0
+    */
 
     cout << "Hello, World!" << endl;
     return 0;
