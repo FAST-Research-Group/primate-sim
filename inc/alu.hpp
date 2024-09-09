275e3f1e65e32f13f3a13343def58114b5dee3c6
Nathan L <nathayoha@gmail.com>
2024-09-08 23:46:36 -0500

    processInstruction

--- inc/alu.hpp
+++ inc/alu.hpp
@@ -1,21 +1,21 @@
 #include "FunctionalUnit.hpp"
 
 class BranchUnit : public FunctionalUnit {
- private:
-  int32_t pc;
+  private:
+    int32_t pc;
 
- public:
-  BranchUnit();
-  ~BranchUnit();
-  int32_t j(int32_t imm);
-  int32_t jr(int32_t imm);
-  int32_t beq(int32_t a, int32_t b);
-  int32_t bne(int32_t a, int32_t b);
-  int32_t blt(int32_t a, int32_t b);
-  int32_t bge(int32_t a, int32_t b);
-  int32_t bltu(int32_t a, uint32_t b);
-  int32_t bgeu(int32_t a, uint32_t b);
-  int32_t end();
+  public:
+    BranchUnit();
+    ~BranchUnit();
+    int32_t j(int32_t imm);
+    int32_t jr(int32_t imm);
+    int32_t beq(int32_t a, int32_t b);
+    int32_t bne(int32_t a, int32_t b);
+    int32_t blt(int32_t a, int32_t b);
+    int32_t bge(int32_t a, int32_t b);
+    int32_t bltu(int32_t a, uint32_t b);
+    int32_t bgeu(int32_t a, uint32_t b);
+    int32_t end();
 };
 
 BranchUnit::BranchUnit() { pc = 0; }
@@ -41,41 +41,43 @@ int32_t BranchUnit::bgeu(int32_t a, uint32_t b) { return a >= b ? 1 : 0; }
 int32_t BranchUnit::end() { return 0; }
 
 class ALU : public FunctionalUnit {
- private:
-  int32_t sign_extend(int32_t imm, int32_t size) {
-    int32_t mask = 1 << (size - 1);
-    return (imm ^ mask) - mask;
-  }
-  int32_t pc;
+  private:
+    int32_t sign_extend(int32_t imm, int32_t size) {
+        int32_t mask = 1 << (size - 1);
+        return (imm ^ mask) - mask;
+    }
+    int32_t pc;
 
- public:
-  ALU();
-  ~ALU();
-  // Arithmetic operations
-  int32_t add(int32_t a, int32_t b);
-  int32_t addi(int32_t a, int32_t imm);
-  int32_t sub(int32_t a, int32_t b);
-  int32_t lui(int32_t imm);
+  public:
+    ALU();
+    ~ALU();
+    // Arithmetic operations
+    int32_t add(int32_t a, int32_t b);
+    int32_t addi(int32_t a, int32_t imm);
+    int32_t sub(int32_t a, int32_t b);
+    int32_t lui(int32_t imm);
 
-  // Logical operations
-  int32_t slti(int32_t a, int32_t imm);
-  int32_t sltiu(int32_t a, uint32_t imm);
-  int32_t andi(int32_t a, int32_t imm);
-  int32_t ori(int32_t a, int32_t imm);
-  int32_t xori(int32_t a, int32_t imm);
-  int32_t snez(int32_t a);  // Set if not zero
-  int32_t and_op(int32_t a, int32_t b);
-  int32_t or_op(int32_t a, int32_t b);
-  int32_t xor_op(int32_t a, int32_t b);
-  int32_t not_op(int32_t a);
+    // Logical operations
+    int32_t slti(int32_t a, int32_t imm);
+    int32_t sltiu(int32_t a, uint32_t imm);
+    int32_t andi(int32_t a, int32_t imm);
+    int32_t ori(int32_t a, int32_t imm);
+    int32_t xori(int32_t a, int32_t imm);
+    int32_t snez(int32_t a); // Set if not zero
+    int32_t and_op(int32_t a, int32_t b);
+    int32_t or_op(int32_t a, int32_t b);
+    int32_t xor_op(int32_t a, int32_t b);
+    int32_t not_op(int32_t a);
 
-  // Shift operations
-  int32_t slli(int32_t a, int32_t shift);
-  int32_t srli(int32_t a, int32_t shift);
-  int32_t srai(int32_t a, int32_t shift);
-  int32_t sll(int32_t a, int32_t b);
-  int32_t srl(int32_t a, int32_t b);
-  int32_t sra(int32_t a, int32_t b);
+    // Shift operations
+    int32_t slli(int32_t a, int32_t shift);
+    int32_t srli(int32_t a, int32_t shift);
+    int32_t srai(int32_t a, int32_t shift);
+    int32_t sll(int32_t a, int32_t b);
+    int32_t srl(int32_t a, int32_t b);
+    int32_t sra(int32_t a, int32_t b);
+
+    void processInstruction(Instruction &I, MachineState &MS);
 };
 
 ALU::ALU() { pc = 0; }
@@ -115,8 +117,8 @@ int32_t ALU::slli(int32_t a, int32_t shift) { return a << shift; }
 int32_t ALU::srli(int32_t a, int32_t shift) { return a >> shift; }
 
 int32_t ALU::srai(int32_t a, int32_t shift) {
-  int32_t mask = 1 << 31;
-  return (a >> shift) | (mask >> shift);
+    int32_t mask = 1 << 31;
+    return (a >> shift) | (mask >> shift);
 }
 
 int32_t ALU::sll(int32_t a, int32_t b) { return a << b; }
@@ -124,6 +126,6 @@ int32_t ALU::sll(int32_t a, int32_t b) { return a << b; }
 int32_t ALU::srl(int32_t a, int32_t b) { return a >> b; }
 
 int32_t ALU::sra(int32_t a, int32_t b) {
-  int32_t mask = 1 << 31;
-  return (a >> b) | (mask >> b);
+    int32_t mask = 1 << 31;
+    return (a >> b) | (mask >> b);
 }
