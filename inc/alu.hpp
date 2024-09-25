3e18fc481e46ee35fb189e3213fc1c9a6a01f551
MirceaNT <mirceatatulescu@utexas.edu>
2024-09-24 21:41:23 -0500

    Branch unit complete?

--- inc/alu.hpp
+++ inc/alu.hpp
@@ -1,10 +1,11 @@
 #include "FunctionalUnit.hpp"
 
-class BranchUnit : public FunctionalUnit {
- private:
+class BranchUnit : public FunctionalUnit
+{
+private:
   int32_t pc;
 
- public:
+public:
   BranchUnit();
   ~BranchUnit();
   int32_t j(int32_t imm);
@@ -16,6 +17,7 @@ class BranchUnit : public FunctionalUnit {
   int32_t bltu(int32_t a, uint32_t b);
   int32_t bgeu(int32_t a, uint32_t b);
   int32_t end();
+  void processInstruction(Instruction &I, MachineState &MS) override;
 };
 
 BranchUnit::BranchUnit() { pc = 0; }
@@ -40,15 +42,45 @@ int32_t BranchUnit::bgeu(int32_t a, uint32_t b) { return a >= b ? 1 : 0; }
 
 int32_t BranchUnit::end() { return 0; }
 
-class ALU : public FunctionalUnit {
- private:
-  int32_t sign_extend(int32_t imm, int32_t size) {
+void BranchUnit::processInstruction(Instruction &I, MachineState &MS)
+{
+  switch (I.get_type())
+  {
+  case 'J':
+    MS.setPC(MS.getPC() + I.get_immediate());
+    break;
+  case 'B':
+    switch (I.get_funct3())
+    {
+    case 0:
+      (I.get_rs1() == I.get_rs2()) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    case 1:
+      (I.get_rs1() != I.get_rs2()) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    case 4:
+      (I.get_rs1() < I.get_rs2()) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    case 6:
+      (((unsigned)I.get_rs1()) < ((unsigned)I.get_rs2())) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    case 5:
+      (I.get_rs1() >= I.get_rs2()) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    case 7:
+      (((unsigned)I.get_rs1()) >= ((unsigned)I.get_rs2())) ? MS.setPC(MS.getPC() + I.get_immediate()) : MS.setPC(MS.getPC() + 4);
+    }
+  default:
+    MS.setPC(MS.getPC() + 4);
+  }
+}
+
+class ALU : public FunctionalUnit
+{
+private:
+  int32_t sign_extend(int32_t imm, int32_t size)
+  {
     int32_t mask = 1 << (size - 1);
     return (imm ^ mask) - mask;
   }
   int32_t pc;
 
- public:
+public:
   ALU();
   ~ALU();
   // Arithmetic operations
@@ -63,7 +95,7 @@ class ALU : public FunctionalUnit {
   int32_t andi(int32_t a, int32_t imm);
   int32_t ori(int32_t a, int32_t imm);
   int32_t xori(int32_t a, int32_t imm);
-  int32_t snez(int32_t a);  // Set if not zero
+  int32_t snez(int32_t a); // Set if not zero
   int32_t and_op(int32_t a, int32_t b);
   int32_t or_op(int32_t a, int32_t b);
   int32_t xor_op(int32_t a, int32_t b);
@@ -114,7 +146,8 @@ int32_t ALU::slli(int32_t a, int32_t shift) { return a << shift; }
 
 int32_t ALU::srli(int32_t a, int32_t shift) { return a >> shift; }
 
-int32_t ALU::srai(int32_t a, int32_t shift) {
+int32_t ALU::srai(int32_t a, int32_t shift)
+{
   int32_t mask = 1 << 31;
   return (a >> shift) | (mask >> shift);
 }
@@ -123,7 +156,8 @@ int32_t ALU::sll(int32_t a, int32_t b) { return a << b; }
 
 int32_t ALU::srl(int32_t a, int32_t b) { return a >> b; }
 
-int32_t ALU::sra(int32_t a, int32_t b) {
+int32_t ALU::sra(int32_t a, int32_t b)
+{
   int32_t mask = 1 << 31;
   return (a >> b) | (mask >> b);
 }
