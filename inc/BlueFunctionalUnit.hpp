17b281b511f79a4f36be8aec0382b5d8804debe2
Code-Tomato <nathayoha@gmail.com>
2024-10-18 19:35:56 -0500

    input done

--- inc/BlueFunctionalUnit.hpp
+++ inc/BlueFunctionalUnit.hpp
@@ -3,15 +3,42 @@
 
 #include "FunctionalUnit.hpp"
 
-class BlueFunctionalUnit : public FunctionalUnit {
-  public:
-    BlueFunctionalUnit();
-    virtual ~BlueFunctionalUnit();
+#define BFU_INPUT 11
+#define BFU_OUTPUT 1
 
+/**
+ * @brief This class controls the code for the custom instructions in primate
+ */
+class BFU : public FunctionalUnit {
+  public:
+    BFU();
+    virtual ~BFU();
+
+    /**
+     * @brief This handles the main process to run custom instructions
+     * @param I The current instruction
+     * @param MS The current state of the machine
+     */
     void processInstruction(Instruction &I, MachineState &MS) override;
 
   private:
-    void handleBluematch(Instruction &I, MachineState &MS);
+    void handleInput(Instruction &I, MachineState &MS);
+    void handleInputRead(Instruction &I, MachineState &MS);
+    void handleOutputEmiti(Instruction &I, MachineState &MS);
+
+    /* Not implimented
+
+    INPUT INSTRUCTIONS
+      void handleInputExtracti(Instruction &I, MachineState &MS);
+      void handleInputExtractiDone(Instruction &I, MachineState &MS);
+      void handleInputSeek(Instruction &I, MachineState &MS);
+      void handleInputDone(Instruction &I, MachineState &MS);
+
+    OUTPUT INSTRUCTIONS
+      void handleOutputMeta(Instruction &I, MachineState &MS);
+      void handleOutputEmitiDone(Instruction &I, MachineState &MS);
+      void handleOutputDone(Instruction &I, MachineState &MS);
+    */
 };
 
 #endif // BLUE_FUNCTIONAL_UNIT_HPP
--- src/BlueFunctionalUnit.cpp
+++ src/BlueFunctionalUnit.cpp
@@ -2,27 +2,64 @@
 #include <iostream>
 
 // Constructor
-BlueFunctionalUnit::BlueFunctionalUnit() {}
+BFU::BFU() {}
 
 // Destructor
-BlueFunctionalUnit::~BlueFunctionalUnit() {}
+BFU::~BFU() {}
 
-void BlueFunctionalUnit::processInstruction(Instruction &I, MachineState &MS) {
-    // Check the opcode or identifier for blue instructions in I
-    // and call the respective handler method
+void BFU::processInstruction(Instruction &I, MachineState &MS) {
     switch(I.get_opcode()) {
-    case 1: // Opcode::BLUEMATCH:
-        handleBluematch(I, MS);
+    case BFU_INPUT:
+        handleInput(I, MS);
+        break;
+    case BFU_OUTPUT:
+        handleOutputEmiti(I, MS);
         break;
     default:
-        std::cerr << "Unknown blue instruction encountered" << std::endl;
+        throw std::string("Unknown custom intruction.");
         break;
     }
 }
 
-void BlueFunctionalUnit::handleBluematch(Instruction &I, MachineState &MS) {
-
-    Register rd = I.get_rd();
-    Register rs1 = I.get_rs1();
-    int immediate = I.get_immediate();
+void BFU::handleInput(Instruction &I, MachineState &MS) {
+    switch(I.get_funct3()) {
+    case 1:
+        handleInputRead(I, MS);
+        break;
+    case 2:
+        // handleInputSeek not implemented
+        throw std::string("inputSeek is not an implemented instruction.");
+        break;
+    case 3:
+        throw std::string("inputExtracti is not an implemented instruction.");
+        break;
+    case 4:
+        throw std::string("inputDone is not an implemented instruction.");
+        break;
+    case 7:
+        throw std::string(
+            "inputExtractiDone is not an implemented instruction.");
+        break;
+    case 5:
+    case 6:
+    default:
+        throw std::string("Not a custom instruction.");
+        break;
+    }
 }
+
+void BFU::handleInputRead(Instruction &I, MachineState &MS) {
+    int rd = I.get_rd();
+    int imm = I.get_immediate();
+    int holdInput = 0;
+    std::string temp = MS.inputStream.str();
+    for(int i = 0; i < imm; ++i)
+        holdInput += (temp[i] - '0') << i;
+    MS.setRegister(rd, holdInput);
+    temp.erase(0, imm);
+    MS.inputStream.str("");
+    MS.inputStream.clear();
+    MS.inputStream << temp;
+}
+
+void handleOutputEmiti(Instruction &I, MachineState &MS) {}
