9540f86587b295f6358d31ccc1276fb4f975616b
Mircea <mirceatatulescu@utexas.edu>
2024-10-27 13:55:56 -0500

    Instructions are reveresed after being parsed, so branch should be last

--- inc/PrimateConfig.hpp
+++ inc/PrimateConfig.hpp
@@ -339,8 +339,7 @@ public:
       instrLayout.push_back(FunctionalUnitType::BFU);
       bfuTemp--;
     }
-    instrLayout.insert(instrLayout.begin(), FunctionalUnitType::BRANCH);
-
+    instrLayout.push_back(FunctionalUnitType::BRANCH);
     instruction_width = instrLayout.size();
   }
 
