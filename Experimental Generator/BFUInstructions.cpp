86b7c107bcfec1f4e00078450d262043bfdf34bc
Mircea <mirceatatulescu@utexas.edu>
2024-11-16 14:59:05 -0600

    Modified generator so that it's possible to read from interconnect for merged unit

--- Experimental Generator/BFUInstructions.cpp
+++ Experimental Generator/BFUInstructions.cpp
@@ -7,19 +7,19 @@
 // #include "io.hpp"
 
 extern "C" void generateMaps(
-    std::map<int, void (*)(Instruction &I, MachineState &CMS, MachineState &NMS)> &indexToFunction,
-    std::map<void (*)(Instruction &I, MachineState &CMS, MachineState &NMS), std::string> &functionToName,
+    std::map<int, void (*)(Instruction &I, MachineState &CMS, MachineState &NMS, int &index)> &indexToFunction,
+    std::map<void (*)(Instruction &I, MachineState &CMS, MachineState &NMS, int &index), std::string> &functionToName,
     std::map<std::string, int> &nameToIndex)
 {
     // Populate indexToFunction map
     indexToFunction[0] = MatMul::getFunctionPTR();
-    indexToFunction[1] = lsu::getFunctionPTR();
-    indexToFunction[2] = io::getFunctionPTR();
+    // indexToFunction[1] = lsu::getFunctionPTR();
+    // indexToFunction[2] = io::getFunctionPTR();
 
     // Populate functionToName map
     functionToName[MatMul::getFunctionPTR()] = "MatMul";
-    functionToName[lsu::getFunctionPTR()] = "lsu";
-    functionToName[io::getFunctionPTR()] = "io";
+    // functionToName[lsu::getFunctionPTR()] = "lsu";
+    // functionToName[io::getFunctionPTR()] = "io";
 
     // Populate nameToIndex map
     nameToIndex["MatMul"] = 0;
