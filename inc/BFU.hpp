d527236836ba12b3ffef2cac16c1b40c51ac9184
Mircea <mirceatatulescu@utexas.edu>
2024-11-16 12:35:31 -0600

    Updated Merged Unit to take BFU and wrote BFU class to process function pointer instructions

--- inc/BFU.hpp
+++ inc/BFU.hpp
@@ -1,10 +1,16 @@
 #pragma once
-
+#include "MachineState.hpp"
 #include "FunctionalUnit.hpp"
 
 class BFU : public FunctionalUnit
 {
-    BFU(bool, unsigned);
+private:
+    uint64_t pc;
+    std::string name;
+
+public:
+    BFU(std::string, bool, unsigned);
     virtual ~BFU();
     void processInstruction(Instruction &I, MachineState &CMS, MachineState &NMS) override;
+    std::string getName();
 };
---
+++ src/BFU.cpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include "../inc/BFU.hpp"
+#include "../src/main.cpp"
+
+BFU::BFU(std::string func, bool reg, unsigned slot) : name(func), FunctionalUnit(reg, slot) { pc = 0; }
+BFU::~BFU() {};
+std::string BFU::getName()
+{
+    return this->name;
+}
+
+void BFU::processInstruction(Instruction &I, MachineState &CMS, MachineState &NMS)
+{
+    indexToFunction[this->slotIdx](I, CMS, NMS);
+}
